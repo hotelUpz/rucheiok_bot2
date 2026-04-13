e4dd27a0d2fe0544dd9c8fde766a09299a6943cf
Sonnik9 <nikolas.smsttt@gmail.com>
2026-04-14 01:24:18 +0300

    adlfjgn'

--- CORE/orchestrator.py
+++ CORE/orchestrator.py
@@ -417,7 +417,8 @@ class TradingBot:
         logger.info("🔄 Прогрев кэша цен и фандинга...")
         await self.price_manager.warmup()
         self._price_updater_task = asyncio.create_task(self.price_manager.loop())
-        asyncio.create_task(self.funding_manager.run())
+        self._await_task(getattr(self, '_funding_task', None))
+        self._funding_task = asyncio.create_task(self.funding_manager.run())
         await asyncio.sleep(1)
         
         self._private_ws_task = asyncio.create_task(self.private_ws.run(self.ws_handler.process_phemex_message))
